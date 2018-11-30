37a5b84158310774fb1cddda76a54d3e57c145d9
Andrew Hinojosa <drewsview34@gmail.com>
2018-11-30 13:21:38 -0800

    regex 2/7

--- code-challenges/301/regular-expressions01/challenges-04.test.js
+++ code-challenges/301/regular-expressions01/challenges-04.test.js
@@ -26,11 +26,11 @@ Return an array containing all the matches.
 ------------------------------------------------------------------------------------------------ */
 
 const isCapitalized = (str) => {
-  let regex = /[A-Z][a-z]*/g;
+  let regex = (/[A-Z][a-z]*/g);
   return str.match(regex);
 };
 
-/* ------------------------------------------------------------------------------------------------
+/* -------------------------------------------------------------------------------------------------
 CHALLENGE 3
 
 Write a function named citiesAtoJ that takes in an array of city names and uses a regular expression pattern to return a new array containing any cities that begin with the letters A through J, inclusive.
